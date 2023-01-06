260247b5af92f4e7ca381c0c6d267655f42919c7
Zach Bush <bus21008@byui.edu>
2023-01-06 09:39:53 -0700

    Prep 2 work

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+       Console.Write("What was your grade percent in the class?: ") ;
+       string grade = Console.ReadLine();
+       int gradeInt = int.Parse(grade);
+
+       string letter = "";
+
+       if (gradeInt >= 90 )
+       {
+            letter = "A";
+       }
+        else if (gradeInt >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeInt >= 70)
+        {
+             letter = "C";
+            
+        }
+        else if (gradeInt >= 60)
+        {
+            letter = "D";
+            
+        }
+        else
+        {
+             letter = "F";
+            
+        }
+
+        Console.WriteLine($"You got a {letter}");
+    
+        if (gradeInt >= 70)
+        {
+            Console.WriteLine("You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("You failed the class.");
+        }
     }
 }
