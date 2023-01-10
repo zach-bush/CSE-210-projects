d51f16df9f176d6d1c03fd5dbfd37e068cbdf10d
Zach Bush <bus21008@byui.edu>
2023-01-10 15:50:27 -0700

    Week 2 prepare work

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
-    }   
+        Job job1 = new Job();
+        job1._jobTitle = "Programmer";
+        job1._company = "Walmart";
+        job1._startYear = 2014;
+        job1._endYear = 2021;
+        
+
+        Job job2 = new Job();
+        job2._jobTitle = "Researcher";
+        job2._company = "Biolabs";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+        
+    
+        Resume myResume = new Resume();
+        myResume._name = "Zachary Bush";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+        myResume.Display();
+
+    
+    
+    
+    }
+
+      
 }
